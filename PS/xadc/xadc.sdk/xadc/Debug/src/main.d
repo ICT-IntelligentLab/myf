905f644d4e48fd6a7e0b5ee23d1e63c61fa865b6
Flavius-myf <2063167447@qq.com>
2025-12-09 15:19:03 +0800

    PS

---
+++ PS/xadc/xadc.sdk/xadc/Debug/src/main.d
@@ -0,0 +1,46 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xadcps.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xadcps_hw.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../xadc_bsp/ps7_cortexa9_0/include/sleep.h
+
+../../xadc_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xadcps.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xadcps_hw.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../xadc_bsp/ps7_cortexa9_0/include/sleep.h:
